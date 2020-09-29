9cb1b944ede503f5b7c205ca3a69d9a492074852
Audrius Sultonas <50612630+kittuh@users.noreply.github.com>
2020-09-29 09:06:53 +0300

    Update fourth.rb

--- 2pd/fourth.rb
+++ 2pd/fourth.rb
@@ -1,13 +1,16 @@
+#Klase kuri apskaiciuoja skaicisu faktoriala
 class Integer
   def fact
+    #nuo 1 iki pasirinkto skaiciaus
+    #Kiekviena iteracija skaicius yra padauginamas
     (1..self).reduce(:*) || 1
   end
 end
-
+#Nuskaitomas skaicius is consolines eilutes
 require "readline"
 firstNr=0
 puts "Input number:"
 firstNr = gets
 
-
+#Parasomas atsakymas skaicium kreipiantis i klases Integer Fact metoda.
 puts firstNr.to_i.fact
